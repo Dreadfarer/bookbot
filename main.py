79a58094cfeb2c4c4dfb67c7d9abf6128ac1ce74
Dreadfarer <niraiya@gmail.com>
2025-09-24 16:07:32 -0400

    edited

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from stats import count
+from stats import count_characters
+
+char_count = count_characters()
 
 count()
+
+print(char_count)
--- stats.py
+++ stats.py
@@ -1,9 +1,21 @@
 def get_book_text(path_to_file):
     with open(path_to_file) as file:
         return file.read()
+    
+path = '/home/dreadfarer/github.com/Dreadfarer/bookbot/books/frankenstein.txt'
+book_text=get_book_text(path)
+words = book_text.split()
 
 def count():
-    path = '/home/dreadfarer/github.com/Dreadfarer/bookbot/books/frankenstein.txt'
-    book_text=get_book_text(path)
-    words = book_text.split()
     print(f'Found {len(words)} total words')
+
+def count_characters():
+    dict = {}
+    for word in words:
+        lowered_char = word.lower()
+        for char in lowered_char:
+            if char in dict:
+                dict[char] += 1
+            else:
+                dict[char] = 1
+    return dict
