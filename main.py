5a839a659e3051b0c5dea32fe2d7cf4305ec835b
Dreadfarer <niraiya@gmail.com>
2025-09-24 15:40:35 -0400

    edited main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ def get_book_text(path_to_file):
 def main():
     path = '/home/dreadfarer/github.com/Dreadfarer/bookbot/books/frankenstein.txt'
     book_text=get_book_text(path)
-    print(book_text)
+    words = book_text.split()
+    print(f'Found {len(words)} total words')
 
 main()
