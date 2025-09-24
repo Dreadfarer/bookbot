028fd1564df8b94f929b1761939739ba429af1c2
Dreadfarer <niraiya@gmail.com>
2025-09-24 15:49:23 -0400

    fixed

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-def get_book_text(path_to_file):
-    with open(path_to_file) as file:
-        return file.read()
+from stats import count
 
-def main():
-    path = '/home/dreadfarer/github.com/Dreadfarer/bookbot/books/frankenstein.txt'
-    book_text=get_book_text(path)
-    words = book_text.split()
-    print(f'Found {len(words)} total words')
-
-main()
+count()
