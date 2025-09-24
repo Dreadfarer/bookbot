9c266f2d13e45383ae7666dd327de2f3ca6ca325
Dreadfarer <niraiya@gmail.com>
2025-09-24 15:34:34 -0400

    Wrote the right main.py codes

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as file:
+        return file.read()
+
+def main():
+    path = '/home/dreadfarer/github.com/Dreadfarer/bookbot/books/frankenstein.txt'
+    book_text=get_book_text(path)
+    print(book_text)
+
+main()
